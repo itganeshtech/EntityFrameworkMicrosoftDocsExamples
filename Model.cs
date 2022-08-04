a7c3b8f6b2d57c421f3c501e8e38f778c48c6406
itganeshtech <it.ganesh.tech@gmail.com>
2022-08-04 21:39:24 +0530

    Deleting name and rating in blog class.

--- Model.cs
+++ Model.cs
@@ -32,8 +32,8 @@ public class Blog
     }
     public int BlogId { get; set; }
     public string Url { get; set; }
-    public string Name { get; set; }
-    public int rating { get; set; }
+   // public string Name { get; set; }
+   // public int rating { get; set; }
 
     public List<Post> Posts { get; }
 }
